29131c3dd21ff3d7326961d5efe396b3e7c7a98c
charangollakoti <166940086+charangollakoti@users.noreply.github.com>
2024-04-13 22:01:59 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Home from "./Components/Home"
+import Blog from "./Components/Blog"
+import About from "./Components/About"
+import Register from "./Components/Register"
+import Login from "./Components/Login"
+import Login1 from "./Components/Login1"
+import Forgotpass from "./Components/Forgotpass"
+import Resetpass from "./Components/Resetpass"
+import Registerbeta from "./Components/Registerbeta"
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import './App.css'
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/beta" element={<Registerbeta />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/login1" element={<Login1 />}/>
+        <Route path="/home" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/forgotpass" element={<Forgotpass />} />
+        <Route path="/resetpass/:token" element={<Resetpass />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/Components/Home.jsx
@@ -0,0 +1,83 @@
+import logo from "../assets/logo.png";
+import logo2 from "../assets/logo2.png";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([]); 
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/getdata")
+      .then((response) => {
+        console.log(response.data);
+        setBlogs(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="main">
+      <nav>
+        <div className="div1">
+          <img src={logo} alt="Logo" className="logo" />
+          <div className="div2">
+            <Link to="/" className="link">
+              <h3>Home</h3>
+            </Link>
+            <Link to="/blog" className="link">
+              <h3>Create a Blog</h3>
+            </Link>
+            <Link to="/about" className="link">
+              <h3>About</h3>
+            </Link>
+          </div>
+        </div>
+      </nav>
+      <div className="big-font">
+        <center>
+          <h1>Read or Create Your</h1>
+          <h1>ideas💡 here !</h1>
+        </center>
+      </div>
+      <center>
+        {blogs.length === 0 ? (
+          <div className="no-blogs">
+            <center>
+              <h2 className="first-author">Be the first author of</h2>
+              <img src={logo2} alt="" className="logo2" />
+            </center>
+            <Link to="/blog" className="link">
+              <button className="btn-12">
+                <span>Create a Blog</span>
+              </button>
+            </Link>
+          </div>
+        ) : (
+          blogs.map((blog) => (
+            <div className="blog" key={blog._id}>
+              <center>
+                <h2>{blog.heading}</h2>
+              </center>
+              <div className="text">
+                <p>{blog.blog}</p>
+              </div>
+              <div className="contacts">
+                <p>
+                  <strong>Created by {blog.author}</strong>
+                </p>
+                <br />
+                <p>
+                  Contact {blog.author} at {blog.email}
+                </p>
+              </div>
+            </div>
+          ))
+        )}
+      </center>
+    </div>
+  );
+};
+
+export default Home;
